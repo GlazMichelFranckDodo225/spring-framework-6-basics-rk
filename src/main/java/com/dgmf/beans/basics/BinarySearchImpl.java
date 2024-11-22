96e277d2eb3efdbf26239b7499323f82947a5f8e
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-22 14:06:38 +0100

    Lifecycle of a Bean - @PostConstruct and @PreDestroy

--- src/main/java/com/dgmf/beans/basics/BinarySearchImpl.java
+++ src/main/java/com/dgmf/beans/basics/BinarySearchImpl.java
@@ -1,5 +1,9 @@
 package com.dgmf.beans.basics;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -8,8 +12,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 // @Scope("prototype")
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+// @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
+    private Logger logger = LoggerFactory.getLogger(BinarySearchImpl.class);
+
     @Autowired
     @Qualifier("bubble")
     private SortAlgorithm sortAlgorithm;
@@ -19,5 +26,17 @@ public class BinarySearchImpl {
         System.out.println(sortAlgorithm);
 
         return 3;
+    }
+
+    // Called as soon as Bean is been Created
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("Into postConstruct() Method");
+    }
+
+    // Called before Bean has been Removed from Container
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("Into preDestroy() Method");
     }
 }

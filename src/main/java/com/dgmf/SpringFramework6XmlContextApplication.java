000a1a53f740447f414f8f8c077125fb465bb028
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-30 04:53:17 +0100

    Defining Spring Application Context using XML - V2

--- src/main/java/com/dgmf/SpringFramework6XmlContextApplication.java
+++ src/main/java/com/dgmf/SpringFramework6XmlContextApplication.java
@@ -1,14 +1,8 @@
 package com.dgmf;
 
-import com.dgmf.beans.basics.BinarySearchImpl;
 import com.dgmf.beans.xmlconfig.XmlPersonDAO;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringFramework6XmlContextApplication {
 	public static void main(String[] args) {
 		try(ClassPathXmlApplicationContext xmlApplicationContext =

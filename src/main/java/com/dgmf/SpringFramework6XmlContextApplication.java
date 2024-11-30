af40afcb912e630fa87c2d220b5efe3c823368ad
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-11-30 05:01:04 +0100

    Defining Spring Application Context using XML - V3

--- src/main/java/com/dgmf/SpringFramework6XmlContextApplication.java
+++ src/main/java/com/dgmf/SpringFramework6XmlContextApplication.java
@@ -1,9 +1,13 @@
 package com.dgmf;
 
 import com.dgmf.beans.xmlconfig.XmlPersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringFramework6XmlContextApplication {
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringFramework6XmlContextApplication.class);
+
 	public static void main(String[] args) {
 		try(ClassPathXmlApplicationContext xmlApplicationContext =
 					new ClassPathXmlApplicationContext(
@@ -15,6 +19,11 @@ public class SpringFramework6XmlContextApplication {
 
 			System.out.println(xmlPersonDAO);
 			System.out.println(xmlPersonDAO.getXmlJdbcConnection());
+
+			LOGGER.info(
+					"{} ||| {}",
+					xmlPersonDAO, xmlPersonDAO.getXmlJdbcConnection()
+			);
 		}
 	}
 
